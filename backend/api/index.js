ffde2398e93d123f7ed6cddbd29ae928e2cab548
novaotp <rahmansajidur30@gmail.com>
2023-08-25 17:59:49 +0200

    Added express, dotenv/config and a post controller for signup

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,6 @@
 
 import express from 'express';
+import 'dotenv/config'
 
 const app = express();
 
@@ -7,6 +8,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/login', (req, res) => {
+  const body = req.body;
+
+  res.status(200).json();
+
+  res.end();
+});
+
+app.post('/signup', (req, res) => {
   const body = req.body;
 
   res.status(200).json();
