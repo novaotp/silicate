8a8636ae3d8acbfb77a8a196ff6e24b9c00c4123
novaotp <rahmansajidur30@gmail.com>
2023-08-26 17:16:29 +0200

    Added a <new> postgresql script for db creation

---
+++ backend/databases/postgresql/new.sql
@@ -0,0 +1,12 @@
+DROP SCHEMA IF EXISTS public;
+CREATE SCHEMA public;
+
+CREATE TABLE public.accounts (
+  id SERIAL NOT NULL,
+  name VARCHAR(80) NOT NULL,
+  email VARCHAR(80) NOT NULL,
+  password VARCHAR(24) NOT NULL,
+  CONSTRAINT pk_id PRIMARY KEY (id),
+  CONSTRAINT u_email UNIQUE (email),
+  CONSTRAINT c_email CHECK (email LIKE '*@*.*')
+);
