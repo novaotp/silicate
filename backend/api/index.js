c6a852a2e02307e1430559ca3e8e73a78c3b9250
novaotp <rahmansajidur30@gmail.com>
2023-08-27 08:06:18 +0200

    Added login support

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,4 @@
 
-/** @typedef { import('../../shared/interfaces').SignUpProps } SignUpProps */
-
 import express from 'express';
 import 'dotenv/config';
 import client from './databases/postgres/index.js';
@@ -10,17 +8,33 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/login', (req, res) => {
-  const body = req.body;
+app.post('/login', async (req, res) => {
+  try {
+    /** @type { import('../../shared/interfaces').LoginProps } */
+    const body = req.body;
 
-  res.status(200).json();
+    const userQuery = 'SELECT * FROM public.accounts WHERE email = $1 LIMIT 1';
+    const userValues = [body.email];
+    const { rows } = await client.query(userQuery, userValues);
+    
+    if (rows.length === 0) return res.status(400).json({ success: false, message: `User with email "${body.email}" does not exist` });
 
-  res.end();
+    const user = rows[0];
+
+    if (user.password !== body.password) return res.status(400).json({ success: false, message: 'The passwords do not match' });
+
+    return res.status(200).json({ success: true, message: 'Connected successfully' });
+
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ success: false, message: 'Internal Server Error' })
+
+  }
 });
 
 app.post('/signup', async (req, res) => {
   try {
-    /** @type SignUpProps */
+    /** @type { import('../../shared/interfaces').SignUpProps } */
     const body = req.body;
 
     const userExistsQuery = 'SELECT EXISTS(SELECT * FROM public.accounts WHERE email = $1)';
@@ -28,9 +42,7 @@ app.post('/signup', async (req, res) => {
     const { rows } = await client.query(userExistsQuery, userExistsValues);
     const userExists = rows[0].exists;
     
-    if (userExists) {
-      return res.status(400).json({ success: false, message: 'User already exists' });
-    }
+    if (userExists) return res.status(400).json({ success: false, message: 'User already exists' });
 
     const newAccountQuery = 'INSERT INTO public.accounts (name, email, password) VALUES ($1, $2, $3)';
     const newAccountValues = [body.name, body.email, body.password];
