60ad1a9e23765733458820d0a58dfb2f16c4f6fa
ENDERastronaute <ENDERastronaute@gmail.com>
2023-08-29 22:46:22 +0200

    Fixed new.sql

--- backend/api/databases/postgres/scripts/new.sql
+++ backend/api/databases/postgres/scripts/new.sql
@@ -2,7 +2,8 @@
 
 CREATE TABLE IF NOT EXISTS public.user (
   id SERIAL PRIMARY KEY,
-  username VARCHAR(50) NOT NULL,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
   password VARCHAR(50) NOT NULL,
   email VARCHAR(50) NOT NULL
 );
