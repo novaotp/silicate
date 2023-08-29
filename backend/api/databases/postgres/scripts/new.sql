c82a8d193ca46b7d6dd710dd4a111425da089241
novaotp <rahmansajidur30@gmail.com>
2023-08-29 16:29:10 +0200

    Updated new.sql file

--- backend/api/databases/postgres/scripts/new.sql
+++ backend/api/databases/postgres/scripts/new.sql
@@ -2,11 +2,36 @@ DROP SCHEMA IF EXISTS public CASCADE;
 CREATE SCHEMA public;
 
 CREATE TABLE public.accounts (
-  id SERIAL NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(80) NOT NULL,
   email VARCHAR(80) NOT NULL,
   password VARCHAR(24) NOT NULL,
-  CONSTRAINT pk_id PRIMARY KEY (id),
   CONSTRAINT u_email UNIQUE (email),
   CONSTRAINT c_email CHECK (email LIKE '%@%.%')
+);
+
+CREATE TABLE gradebooks (
+  id SERIAL NOT NULL,
+  account_id INT,
+  name TEXT,
+  CONSTRAINT pk_grade_books_id PRIMARY KEY (id),
+  CONSTRAINT fk_grade_books_accounts FOREIGN KEY (account_id) REFERENCES public.accounts (id),
+  CONSTRAINT u_grade_book UNIQUE (account_id, name)
+);
+
+CREATE TABLE subjects (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name TEXT,
+  CONSTRAINT u_subject UNIQUE (name)
+);
+
+CREATE TABLE grades (
+  id SERIAL PRIMARY KEY NOT NULL,
+  gradebook_id INT NOT NULL,
+  subject_id INT NOT NULL,
+  title TEXT,
+  value TEXT,
+  CONSTRAINT fk_grades_gradebooks FOREIGN KEY (gradebook_id) REFERENCES public.gradebooks (id),
+  CONSTRAINT fk_grades_subjects FOREIGN KEY (subject_id) REFERENCES public.subjects (id),
+  CONSTRAINT u_grade UNIQUE (gradebook_id, subject_id, title, value)
 );
