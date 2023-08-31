f5365b5a05f5fe8ef0dde20315531d38eca56aaf
novaotp <rahmansajidur30@gmail.com>
2023-08-31 21:21:34 +0200

    Added search field + dialog popup

---
+++ frontend/src/services/gradebook-service/frontend/components/shared/InputField/index.tsx
@@ -0,0 +1,25 @@
+import { poppins } from '@/core/fonts';
+import styles from './index.module.css';
+
+interface InputFieldProps {
+  type: "text" | "email" | "password";
+  label: string;
+  placeholder: string;
+  name: string;
+}
+
+export default function InputField({ type, label, placeholder, name }: InputFieldProps) {
+  return (
+    <div className={styles.field}>
+      <label htmlFor={name}>{label}</label>
+      <input
+        type={type}
+        className={`${styles.input} ${poppins.className}`}
+        placeholder={placeholder}
+        name={name}
+        autoComplete="off"
+        required={true}
+      />
+    </div>
+  )
+}
