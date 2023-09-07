a1ac5acfe4d717d5e0d4e1ee88f9462cffce4c13
novaotp <rahmansajidur30@gmail.com>
2023-09-07 17:52:46 +0200

    Testing functions in configure files (not working yet)

--- configure.ps1
+++ configure.ps1
@@ -1,8 +1,60 @@
 # Configure the repo (install npm modules etc.)
-Set-Location ./backend/api
-npm i
 
-Set-Location ../../frontend
-npm i
+$root = Get-Location
+function Install-NpmModules ([string] $route, [string] $override) {
+  [string] $prefix = "$route : "
+  [string] $removingMessage = "Removing npm_modules...                  "
+  [string] $installingMessage = "Installing npm_modules...              "
+  [string] $errorMessage = "Oops! Something went wrong. Please check manually.          "
+  [string] $successMessage = "Successfully installed npm modules!     "
+  [string] $skipInstall = "Skipped installation!     "
 
-Set-Location ..
+  Set-Location $route
+
+  if ($override -eq "y") {
+    if (Test-Path "node_modules") {
+      Write-Host -NoNewline "`r$prefix $removingMessage"
+      Remove-Item -Recurse -Force "node_modules"
+    }
+    Write-Host -NoNewline "`r$prefix $installingMessage"
+    npm i *> $null 2>&1
+    if ($LASTEXITCODE -ne 0) {
+      Write-Host "`r$prefix $errorMessage"
+    }
+    else {
+      Write-Host "`r$prefix $successMessage"
+    }
+  }
+  else {
+    if (Test-Path "node_modules") {
+      Write-Host -NoNewline "`r$prefix $skipInstall"
+    }
+    else {
+      Write-Host -NoNewline "`r$prefix No node_modules detected..."
+      Write-Host -NoNewline "`r$prefix $installingMessage"
+      npm i *> $null 2>&1
+      if ($LASTEXITCODE -ne 0) {
+        Write-Host "`r$prefix $errorMessage"
+      }
+      else {
+        Write-Host "`r$prefix $successMessage"
+      }
+    }
+
+    if ($LASTEXITCODE -ne 0) {
+      Write-Host "`r$prefix $errorMessage"
+    }
+  }
+
+  Set-Location $root
+}
+
+Clear-Host
+Write-Host "Starting powershell script..."
+[string] $override = Read-Host "Remove and reinstall the node_modules folder if it already exists ? (y/n) "
+Clear-Host
+Write-Host "Starting powershell script..."
+
+Install-NpmModules -route "backend/api" -override $override
+Install-NpmModules -route "frontend" -override $override
+Install-NpmModules -route "shared" -override $override
--- configure.sh
+++ configure.sh
@@ -1,11 +1,43 @@
 #!/bin/bash
 
-cd ./backend/api || exit
+#!/bin/bash
 
-npm i
+install_npm_modules() {
+  local route=$1
+  local removingMessage="Removing npm_modules from $route..."
+  local installingMessage="Installing npm_modules in $route..."
+  local errorMessage="Oops! Something went wrong while installing npm modules in $route. Please check manually."
+  local successMessage="Successfully installed npm modules in $route !"
 
-cd ../../frontend || exit
+  cd $route
 
-npm i
+  if [ -d "node_modules" ]; then
+    read -p "The node_modules folder already exists in $route. Would you like to delete and reinstall it? (y/n) " response
+    if [ "$response" = "y" ]; then
+      echo $removingMessage
+      rm -rf "node_modules"
+      echo $installingMessage
+      npm i > /dev/null 2>&1
+      if [ $? -ne 0 ]; then
+        echo $errorMessage
+      else
+        echo $successMessage
+      fi
+    else
+      echo "Skipped installing npm modules in $route."
+    fi
+  else
+    echo $installingMessage
+    npm i > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+      echo $errorMessage
+    else
+      echo $successMessage
+    fi
+  fi
 
-cd ..
+  cd -
+}
+
+install_npm_modules "backend/api"
+install_npm_modules "frontend"
