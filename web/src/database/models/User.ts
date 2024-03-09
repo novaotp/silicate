250eadc2c13c7a2c83db61c0db8395de3a871928
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-03-09 04:50:50 +0100

    feat(web): removed database layer in web version

--- web/src/database/models/User.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-    id: number,
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    /** A parsable date. */
-    joinedOn: string,
-}
