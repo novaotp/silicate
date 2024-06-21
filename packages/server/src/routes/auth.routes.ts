986b5b8146a74195d17fe2085b4da661af564ab0
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-06-21 16:21:14 +0200

    fix(server): fixed email already existing error when registering

--- packages/server/src/routes/auth.routes.ts
+++ packages/server/src/routes/auth.routes.ts
@@ -1,63 +1,43 @@
 import { Router } from "express";
-import { db } from "../database";
+import { query } from "../database/utils";
 import { RawUser } from "../../../libs/models/User";
 import { compare, hash } from 'bcrypt';
 import { sign, verify } from '../utils/jwt';
-import { LoginResponse } from '../../../libs/types/AuthResponse';
 
 export const router = Router();
 
 router.post("/login", async (req, res) => {
     try {
         const { email, password } = req.body;
-        const client = await db.connect();
 
-        const { rows } = await client.query<RawUser>(`
+        const { first: user } = await query<RawUser>(`
             SELECT *
             FROM public.user
             WHERE email = $1
             LIMIT 1;
         `, [email]);
 
-        client.release();
-
-        const user = rows[0];
-
         if (!user) {
-            return res.status(404).send({
-                success: false,
-                message: "User not found"
-            } as LoginResponse);
+            return res.notFoundError("User not found");
         }
 
         if (!(await compare(password, user.password))) {
-            return res.status(401).send({
-                success: false,
-                message: "User not found"
-            } as LoginResponse);
+            // 404 to not unveil the existence of a user
+            return res.notFoundError("User not found");
         }
 
         const { token, expires } = await sign({ userId: user.id });
 
-        return res.status(200).send({
-            success: true,
-            message: "Users read successfully",
-            jwt: token,
-            expires
-        } satisfies LoginResponse);
+        return res.success("Users read successfully", { jwt: token, expires });
     } catch (err) {
         console.error(`Something went wrong whilst login a user : ${err.message}`);
-        return res.status(500).send({
-            success: false,
-            message: "Internal Server Error"
-        });
+        return res.serverError();
     }
 });
 
 router.post("/validate", async (req, res) => {
     try {
         const { jwt } = req.body;
-        const client = await db.connect();
 
         const userId = (await verify(jwt)).payload.userId;
 
@@ -68,62 +48,48 @@ router.post("/validate", async (req, res) => {
             });
         }
 
-        const { rows } = await client.query<RawUser>(`
+        const { first: user } = await query<RawUser>(`
             SELECT *
             FROM public.user
             WHERE id = $1
             LIMIT 1;
         `, [userId]);
 
-        client.release();
-
-        const user = rows[0];
-
         if (!user) {
-            return res.status(404).send({
-                success: false,
-                message: "User not found"
-            });
+            return res.notFoundError("User not found");
         }
 
-        return res.status(200).send({
-            success: true,
-            message: "Users read successfully"
-        });
+        return res.success("Users read successfully");
     } catch (err) {
         console.error(`Something went wrong whilst validating a jwt : ${err.message}`);
-        return res.status(500).send({
-            success: false,
-            message: "Internal Server Error"
-        });
+        return res.serverError();
     }
 });
 
 router.post('/register', async (req, res) => {
     try {
         const { firstName, lastName, email, password } = req.body;
-        const client = await db.connect();
 
-        const { rows } = await client.query<RawUser>(`
+        const { first: existingUser } = await query<RawUser>(`
+            SELECT *
+            FROM public.user
+            WHERE email = $1
+            LIMIT 1;
+        `, [email]);
+
+        if (existingUser) {
+            return res.status(409).send({ message: "A user with the given email already exists." })
+        }
+
+        const { first: userId } = await query<{ id: number }>(`
             INSERT INTO public.user (first_name, last_name, email, password)
             VALUES ($1, $2, $3, $4)
             RETURNING id;
         `, [firstName, lastName, email, await hash(password, 15)]);
 
-        client.release();
-
-        const id = rows[0].id;
-
-        return res.send({
-            success: true,
-            message: "User created successfully",
-            data: id
-        });
+        return res.success("User created successfully", userId);
     } catch (err) {
         console.error(`Something went wrong whilst creating a user : ${err.message}`);
-        return res.send({
-            success: false,
-            message: "Internal Server Error"
-        });
+        return res.serverError();
     }
 });
